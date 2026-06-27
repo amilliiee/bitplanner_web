77ed292c34a7b6c80dcdaf37f13e5e38a63a2d24
Amelia <alsylvester@hotmail.com>
2026-06-26 20:04:22 -0700

    functioning search bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import ThemeToggle from "./ThemeToggle.jsx";
+import '../styles/header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggle />
+    </header>
+  )
+}
+
+export default Header;
